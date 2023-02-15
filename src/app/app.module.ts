68a123ddb59de7a88f655b01e5e2d8598f5e6d8c
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2023-02-15 09:04:03 -0500

    creation de mes component avec ng generate component (accueil, produit, detail, nonTrouvee et entete)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProduitComponent } from './produit/produit.component';
+import { DetailComponent } from './detail/detail.component';
+import { EnteteComponent } from './entete/entete.component';
+import { NonTrouveeComponent } from './non-trouvee/non-trouvee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProduitComponent,
+    DetailComponent,
+    EnteteComponent,
+    NonTrouveeComponent
   ],
   imports: [
     BrowserModule,
