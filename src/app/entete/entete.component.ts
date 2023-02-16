ad776acfe613b8f175ca36d46612282a568d6c0c
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2023-02-16 16:36:28 -0500

    ajout de button connection et de fucntion seConnecter dans entete

--- src/app/entete/entete.component.ts
+++ src/app/entete/entete.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 
 export class EnteteComponent {
   titre:String = "Administration"
+  msgConnecter:string = "Se connecter"
+  estConnecter:boolean = false;
+
+  seConnecter(){
+    this.estConnecter = !this.estConnecter;
+    if(this.estConnecter){
+      this.msgConnecter = "Se deconnecter";
+    }else{
+      this.msgConnecter = "Se connecter";
+    }
+  }
 }
